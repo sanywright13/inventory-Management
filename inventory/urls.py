b839baa45299d74861096319e066138ecd17e9a0
sanywright13 <axil.wright@gmail.com>
2023-08-11 22:13:40 +0100

    add users login log out

--- inventory/urls.py
+++ inventory/urls.py
@@ -1,9 +1,13 @@
 from django.contrib import admin
 from django.urls import path,include
-from .views import Index , SignUpView
+from .views import Index , SignUpView 
+from django.contrib.auth import views as auth_views
 app_name='inventory'
 urlpatterns = [
    
     path('',Index.as_view(),name="index"),
-    path('signup/',SignUpView.as_view(),name="signup")
+    path('signup/',SignUpView.as_view(),name="signup"),
+    path('login/',auth_views.LoginView.as_view(template_name='inventory/login.html'),name='login'),
+    path('logout/',auth_views.LogoutView.as_view(template_name='inventory/logout.html'),name='logout')
+    
 ]
--- inventoryManagement/settings.py
+++ inventoryManagement/settings.py
@@ -36,6 +36,8 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'inventory',
+    'crispy_forms',
+    'crispy_bootstrap5',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -130,3 +132,6 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+CRISPY_ALLOWED_TEMPLATE_PACKS = "bootstrap5"
+CRISPY_TEMPLATE_PACK = "bootstrap5"
+LOGIN_REDIRECT_URL='/dashboard'
