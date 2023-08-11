b804861c718e0a62cb356767517f94fa2285c121
sanywright13 <axil.wright@gmail.com>
2023-08-11 13:11:28 +0100

    add urls strecture

--- inventory/views.py
+++ inventory/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
-
+from django.http import HttpRequest
+from django.views.generic.base import TemplateView
 # Create your views here.
+class Index(TemplateView):
+    template_name="inventory/index.html"
