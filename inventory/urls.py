6d9b9fd5954f15ec3be2387241aa9da684ddb8fc
sanywright13 <axil.wright@gmail.com>
2023-08-11 16:29:49 +0100

    add user sign up

--- inventory/urls.py
+++ inventory/urls.py
@@ -1,7 +1,9 @@
 from django.contrib import admin
 from django.urls import path,include
-from .views import Index 
+from .views import Index , SignUpView
+app_name='inventory'
 urlpatterns = [
    
-    path('',Index.as_view(),name="index")
+    path('',Index.as_view(),name="index"),
+    path('signup/',SignUpView.as_view(),name="signup")
 ]
--- inventory/views.py
+++ inventory/views.py
@@ -1,6 +1,27 @@
-from django.shortcuts import render
+from django.shortcuts import render ,redirect
 from django.http import HttpRequest
-from django.views.generic.base import TemplateView
+from django.contrib.auth import login, authenticate
+from django.views.generic.base import TemplateView , View
+from .forms import UserRegisterForm
 # Create your views here.
 class Index(TemplateView):
     template_name="inventory/index.html"
+    
+class SignUpView(View):
+    def get(self,request):
+         form=UserRegisterForm()
+         return render(request,'inventory/signup.html',{
+             'form' :form
+         })
+    def post(self,request):
+        form =UserRegisterForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username=form.cleaned_data['username']
+            password=form.cleaned_data['password1']
+            user=authenticate(username=username,password=password)
+            login(request,user)
+            return redirect('inventory:index')
+        return render(request,'inventory/signup.html',{'form':form})
+        
+            
